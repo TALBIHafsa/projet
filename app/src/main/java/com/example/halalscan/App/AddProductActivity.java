6c7971ee1b6fdcdc5e471fc8b217c0b476eace43
hafsa talbi <hafsatllb@gmail.com>
2024-05-28 22:40:05 +0100

    adding the images of the newProducts to the database

--- app/src/main/java/com/example/halalscan/App/AddProductActivity.java
+++ app/src/main/java/com/example/halalscan/App/AddProductActivity.java
@@ -1,26 +1,89 @@
 package com.example.halalscan.App;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.halalscan.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+
 
 public class AddProductActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_add_product);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+
+        private static final int REQUEST_IMAGE_CAPTURE = 1;
+        private Uri photoURI;
+        private String currentPhotoPath;
+        private StorageReference storageRef;
+    private String currentProductKey;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_add_product);
+            storageRef = FirebaseStorage.getInstance().getReference();
+
+            currentProductKey = FirebaseDatabase.getInstance().getReference("newProducts").push().getKey();
+
+            findViewById(R.id.addPicture).setOnClickListener(v -> takePicture("productImage"));
+            findViewById(R.id.addIngredients).setOnClickListener(v -> takePicture("ingredientsImage"));
+        }
+
+        private void takePicture(String type) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                currentPhotoPath = type; // Use this to differentiate between product and ingredients images
+            }
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                uploadImageToFirebase(imageBitmap, currentPhotoPath);
+            }
+        }
+
+
+    private void uploadImageToFirebase(Bitmap bitmap, String type) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        final StorageReference imageRef = storageRef.child("newProducts/" + currentProductKey + "/" + type + ".jpg");
+        UploadTask uploadTask = imageRef.putBytes(data);
+        uploadTask.addOnFailureListener(exception -> {
+            Toast.makeText(this, "Upload failed: " + exception.getMessage(), Toast.LENGTH_LONG).show();
+        }).addOnSuccessListener(taskSnapshot -> {
+            imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                String imageUrl = uri.toString();
+                updateDatabaseWithImageUrl(imageUrl, type);
+                Toast.makeText(this, "Upload successful", Toast.LENGTH_LONG).show();
+            });
         });
     }
-}
+
+    private void updateDatabaseWithImageUrl(String imageUrl, String type) {
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("newProducts").child(currentProductKey);
+        if (type.equals("productImage")) {
+            databaseRef.child("productImage").setValue(imageUrl);
+        } else if (type.equals("ingredientsImage")) {
+            databaseRef.child("ingredientsImage").setValue(imageUrl);
+        }
+    }}
