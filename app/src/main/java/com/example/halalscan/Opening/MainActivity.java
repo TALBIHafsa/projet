c8256766025ed629c711a2f4fc11338f91467a6d
hafsa talbi <hafsatllb@gmail.com>
2024-05-28 19:10:09 +0100

    ll

--- app/src/main/java/com/example/halalscan/Opening/MainActivity.java
+++ app/src/main/java/com/example/halalscan/Opening/MainActivity.java
@@ -3,26 +3,38 @@ package com.example.halalscan.Opening;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.halalscan.App.home;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.example.halalscan.R;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
-        // Delayed transition to page1 after 5 seconds
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, page1.class);
-                startActivity(intent);
-                finish(); // Optional: Finish MainActivity if you don't want it to be accessible after transition
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                if (currentUser != null) {
+                    // User is signed in, skip instruction pages
+                    Intent homeIntent = new Intent(MainActivity.this, home.class);
+                    startActivity(homeIntent);
+                    finish();
+                } else {
+                    // No user is signed in, show instruction pages
+                    Intent intent = new Intent(MainActivity.this, page1.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
-        }, 3000); // 5000 milliseconds = 5 seconds
+        }, 3000); // Delay for 3 seconds
     }
 }
-
